884f6d5b8c2a364ff96702580eefb7e7e80334bd
Hovo91 <hovomnacakanyan91@gmail.com>
2019-09-10 00:16:02 +0400

    slider

--- Slide/script.js
+++ Slide/script.js
@@ -1,27 +1,22 @@
-let slider = document.getElementById('box');
+let img = document.getElementById('img');
+let slider = document.getElementById('slider');
 let images = [2, 3, 4, 5, 6, 7, 1];
 let i = images.length;
-function nextImage() {
+
+let nextImage = function () {
   if (i < images.length) {
     i++;
   } else {
     i = 1;
   }
-  slider.innerHTML = "<img src="+images[i - 1]+".jpg>";
+  img.innerHTML = "<img src="+images[i - 1]+".jpg>";
 }
-function prewImage() {
+
+let prewImage = function () {
   if (i < images.length + 1 && i > 1) {
     i--;
   } else {
     i = images.length;
   }
-  slider.innerHTML = "<img src="+images[i - 1]+".jpg>";
-}
-let playImage = function () {
-  if (i < images.length) {
-    i++;
-  } else {
-    i = 1;
-  }
-  slider.innerHTML = "<img src="+images[i - 1]+".jpg>";
+  img.innerHTML = "<img src="+images[i - 1]+".jpg>";
 }
