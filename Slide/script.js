4f06f6e1c5257bf6defd232d10b670854b746eb2
Hovo91 <hovomnacakanyan91@gmail.com>
2019-09-10 22:53:51 +0400

    slider

--- Slide/script.js
+++ Slide/script.js
@@ -2,8 +2,9 @@ let img = document.getElementById('img');
 let slider = document.getElementById('slider');
 let images = [2, 3, 4, 5, 6, 7, 1];
 let i = images.length;
+let timer = 0;
 
-let nextImage = function () {
+let nextImage = () => {
   if (i < images.length) {
     i++;
   } else {
@@ -12,11 +13,20 @@ let nextImage = function () {
   img.innerHTML = "<img src="+images[i - 1]+".jpg>";
 }
 
-let prewImage = function () {
+let prewImage = () => {
   if (i < images.length + 1 && i > 1) {
     i--;
   } else {
     i = images.length;
   }
   img.innerHTML = "<img src="+images[i - 1]+".jpg>";
+}
+
+let playPause = () => {
+  if (timer) {
+    clearInterval(timer);
+    timer = null;
+  } else {
+    timer = setInterval(nextImage, 1500);
+  }
 }
